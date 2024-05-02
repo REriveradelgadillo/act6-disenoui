d5fd72a61cc8cf83fe7b6d25380a08789c291e4d
ROBERTO_EMMANUEL_RIVERA_DELGADILLO <a.21308051280538@cbtis128.edu.mx>
2024-05-02 15:16:20 +0000

    actu

--- lib/main.dart
+++ lib/main.dart
@@ -4,39 +4,188 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'Gorras Rivera',
+      home: MyHomePage(title: 'Demo App'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: PageView(
+        children: [
+          //first page
+          Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.network(
+                  "https://raw.githubusercontent.com/REriveradelgadillo/img_FlutterFlow_IOS_6J/main/Act12NavBar_FlutterFlow/cap2.jpg",
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const Center(
+                child: SizedBox(
+                  width: 230,
+                  height: 230,
+                  child: Card(
+                    child: Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Column(
+                          children: [
+                            Text(
+                              "NY Yankees",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            Divider(
+                              thickness: 2,
+                              height: 30,
+                            ),
+                            Icon(Icons.arrow_circle_right),
+                            Text(
+                                "New era\n59Fifty\nColor negra con bordados blancos\nedicion Coperstown")
+                          ],
+                        )),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.network(
+                  "https://raw.githubusercontent.com/REriveradelgadillo/img_FlutterFlow_IOS_6J/main/Act12NavBar_FlutterFlow/cap4.jpg",
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const Center(
+                child: SizedBox(
+                  width: 200,
+                  height: 200,
+                  child: Card(
+                    child: Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Column(
+                          children: [
+                            Text(
+                              "Mara Fun!",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            Divider(
+                              thickness: 2,
+                              height: 25,
+                            ),
+                            Icon(Icons.access_alarm),
+                            Text(
+                                "Keep swiping to check out the fun activities at the Mara,")
+                          ],
+                        )),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.network(
+                  "https://raw.githubusercontent.com/REriveradelgadillo/img_FlutterFlow_IOS_6J/main/Act12NavBar_FlutterFlow/cap5.jpg",
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const Center(
+                child: SizedBox(
+                  width: 200,
+                  height: 200,
+                  child: Card(
+                    child: Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Column(
+                          children: [
+                            Text(
+                              "Mara Fun!",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            Divider(
+                              thickness: 2,
+                              height: 25,
+                            ),
+                            Icon(Icons.ac_unit_sharp),
+                            Text(
+                                "Keep swiping to check out the fun activities at the Mara,")
+                          ],
+                        )),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.network(
+                  "https://raw.githubusercontent.com/REriveradelgadillo/img_FlutterFlow_IOS_6J/main/Act12NavBar_FlutterFlow/cap6.jpg",
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const Center(
+                child: SizedBox(
+                  width: 200,
+                  height: 200,
+                  child: Card(
+                    child: Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Column(
+                          children: [
+                            Text(
+                              "Mara Fun!",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            Divider(
+                              thickness: 2,
+                              height: 25,
+                            ),
+                            Icon(Icons.abc),
+                            Text(
+                                "Keep swiping to check out the fun activities at the Mara,")
+                          ],
+                        )),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
